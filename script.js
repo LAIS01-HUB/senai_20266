554e6d5e444a32267f42ed3b1fccd47110dca997
LAIS01-HUB <rochasegallalais@gmail.com>
2026-01-30 09:20:34 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const { json } = require("express");
+
+async function dolar(bt){
+    
+   const api = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+   const json = await (await api).json();
+    bt.innerHTML = `R$ ${Number(json.USDBRL.bid).toFixed(2).replace(".", ",")}`;
+}
